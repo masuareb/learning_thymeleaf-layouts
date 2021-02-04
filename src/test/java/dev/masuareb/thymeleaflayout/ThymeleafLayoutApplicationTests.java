289b763217dc83c4f3aef0633633893692f68424
Arturo Suárez <masuareb@hotmail.com>
2021-02-03 19:24:02 -0600

    Initial Commit

---
+++ src/test/java/dev/masuareb/thymeleaflayout/ThymeleafLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.masuareb.thymeleaflayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
