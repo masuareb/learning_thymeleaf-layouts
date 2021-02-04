30ac69ec3baa25191972119a0d5d0959363c4a45
Arturo Suárez <masuareb@hotmail.com>
2021-02-03 19:57:54 -0600

    Example controller and template

---
+++ src/main/java/dev/masuareb/thymeleaflayout/ExampleController.java
@@ -0,0 +1,12 @@
+package dev.masuareb.thymeleaflayout;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExampleController {
+    @GetMapping("/example")
+    public String example() {
+        return "example";
+    }
+}
